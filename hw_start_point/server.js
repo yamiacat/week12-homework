ffb29d5696ad675f2a19d05e2ae3a70b91209b4f
Matt Rose <yamiacat@gmail.com>
2017-06-26 16:56:30 +0100

    Add restful routes to API

--- hw_start_point/server.js
+++ hw_start_point/server.js
@@ -8,8 +8,38 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 var Film = require('./client/src/models/film');
 var Review = require('./client/src/models/review');
-var filmsSeedData = require('./client/src/models/films');
+var filmsSeedData = require('./client/src/models/films_seed_data');
 var films = filmsSeedData();
+
+
+//INDEX
+app.get("/api/films", function(req, res) {
+  res.json({data: films});
+});
+
+//SHOW
+app.get("/api/films/:id", function(req, res) {
+  res.json({data: films[req.params.id]});
+});
+
+//CREATE
+app.post("/api/films", function(req, res) {
+  films.push(req.body.film)
+  res.json( {data: films})
+});
+
+//UPDATE
+app.put("/api/films/:id", function(req, res) {
+  films[req.params.id] = req.body.film
+  res.json({data: films});
+})
+
+//DESTROY
+
+app.delete("/api/films/:id", function(req, res) {
+  films.splice([req.params.id], 1);
+  res.json({data: films});
+});
 
 
 
